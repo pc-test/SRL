78caff3cc5ec40aa6b58e1a411598487aa2d47e2
pc-test <39305379+pc-test@users.noreply.github.com>
2018-09-28 14:10:53 +0200

    Add files via upload

---
+++ ScriptOne/Action.c
@@ -0,0 +1,16 @@
+Action()
+{
+	
+	lr_start_transaction("Homepage");
+
+	lr_think_time(10);
+	
+	// Added first comment.
+	// Added second comment.	
+	// Added third comment.	
+	// Added fourth comment.
+
+	lr_end_transaction("Homepage", LR_AUTO);
+
+	return 0;
+}
